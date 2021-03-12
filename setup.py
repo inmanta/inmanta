9ade6ae88dd47c24c27323f306b478565e3d1b57
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2021-03-12 15:13:51 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    version="2020.7.1",
+    version="2021.1",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta",
