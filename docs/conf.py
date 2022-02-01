0698fdea0176da685add29038c4a2c7a6e209e58
Sander Van Balen <git@sandervanbalen.be>
2022-02-01 11:42:16 +0100

    fixed docs config inheritance

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,11 @@ import os
 import sys
 
 # import all names from core conf
-sys.path.append(os.path.dirname(__file__))
-from conf_core import *
+core_conf_file: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "conf_core.py"))
+with open(core_conf_file, "r") as f:
+    code = compile(f.read(), core_conf_file, "exec")
+    # share this file's globals namespace (both for reading and for writing)
+    exec(code, globals())
 
 # override exclude patterns: don't exclude anything for product docs
 exclude_patterns = []
