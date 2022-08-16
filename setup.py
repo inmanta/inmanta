49e66f2ad5720b3ec6e49059577bd0af76b68869
Sander <git@sandervanbalen.be>
2022-08-16 13:05:05 +0200

    Stage oss patch release (#94)
    * Staged OSS patch release
    
    * also bumped version in .bumpversion.cfg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    version="2022.2",
+    version="2022.2.1",
     python_requires=">=3.9",  # also update classifiers
     # Meta data
     name="inmanta",
