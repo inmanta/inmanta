b81c98d2c1b23635f190213e5322cf4297348a81
inmantaci <ci@inmanta.com>
2023-04-05 12:02:00 +0200

    Version bump based on change entries

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    version="2023.1.2",
+    version="2023.2",
     python_requires=">=3.9",  # also update classifiers
     # Meta data
     name="inmanta",
