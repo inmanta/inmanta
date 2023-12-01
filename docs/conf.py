a0208e858f8022bc82ff0f19acf0320ce9e79a37
hugo <hugo.thiolliere@inmanta.com>
2023-12-01 15:49:12 +0100

    [LINT] run pyupgrade 39 + autoflake

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 
 # import all names from core conf:
 #   sphinx injects a global named `tags` into the conf.py namespace. Therefore, simply importing all names from the core config
@@ -11,7 +10,7 @@ import sys
 #   to this module's namespace.
 #   (sphinx implementation for reference: https://github.com/sphinx-doc/sphinx/blob/799385f5558a888d1a143bf703d06b66d6717fe4/sphinx/config.py#L329)
 core_conf_file: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "conf_core.py"))
-with open(core_conf_file, "r") as f:
+with open(core_conf_file) as f:
     code = compile(f.read(), core_conf_file, "exec")
     # share this file's globals namespace (both for reading and for writing)
     exec(code, globals())
