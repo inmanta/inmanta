178e016d742e2c5e6ce50c70a213e5c59537c9b7
Sander Van Balen <git@sandervanbalen.be>
2024-01-22 10:44:13 +0100

    stage RC release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    version="2024.1",
+    version="2024.1.1",
     python_requires=">=3.11",  # also update classifiers
     # Meta data
     name="inmanta",
@@ -26,7 +26,7 @@ setup(
         "Documentation": "https://docs.inmanta.com/community/latest/",
     },
     install_requires=[
-        "inmanta-core==11.0.0",
+        "inmanta-core==11.0.1",
         "inmanta-ui==5.1.0",
     ],
     # explicitly declare packages so setuptools does not attempt auto discovery
