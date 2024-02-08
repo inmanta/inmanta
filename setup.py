cac888ba0e9453ba4ea06ef9fb484520f23f9ad2
Sander Van Balen <git@sandervanbalen.be>
2024-02-08 16:04:56 +0100

    bump version after RC

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    version="2024.1.2",
+    version="2024.1.3",
     python_requires=">=3.11",  # also update classifiers
     # Meta data
     name="inmanta",
