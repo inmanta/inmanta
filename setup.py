12da87ee3b3190c964dbbe06fa3e7a0534e53185
inmantaci <ci@inmanta.com>
2024-10-10 13:20:51 +0200

    Pinned python dependencies in setup.py.

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
         "Documentation": "https://docs.inmanta.com/community/latest/",
     },
     install_requires=[
-        "inmanta-core~=14.0.0.0rc",
-        "inmanta-ui~=5.1.4.0rc",
+        "inmanta-core==14.0.0",
+        "inmanta-ui==5.1.4",
     ],
     # explicitly declare packages so setuptools does not attempt auto discovery
     packages=[],
