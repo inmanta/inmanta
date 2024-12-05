14901055c8496416464d1965d900d15b860585bf
wouter <wouter.deborger@inmanta.com>
2024-12-05 09:54:04 +0100

    Update python lower bound to 3.12 for ISO8 (PR #140)
    # Description
    
    1. Move python to 3.12
    2. Update to be in-line with ISO: I backported a LOT, I hope it doesn't catch fire
    
    closes https://github.com/inmanta/inmanta/issues/137
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     version="2024.4.1",
-    python_requires=">=3.11",  # also update classifiers
+    python_requires=">=3.12",  # also update classifiers
     # Meta data
     name="inmanta",
     description="Inmanta deployment tool",
@@ -18,7 +18,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Topic :: System :: Systems Administration",
         "Topic :: Utilities",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="orchestrator orchestration configurationmanagement",
     project_urls={
